ab9cfeb809a04ebd9ea3e117447f05c4991501b1
Wojciech Zankowski <info@zankowski.pl>
2021-11-13 02:43:24 +0100

    Disable resource test

--- src/test/java/ch/zankowski/ListingResourceTest.java
+++ src/test/java/ch/zankowski/ListingResourceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+@Disabled
 @QuarkusTest
 public class ListingResourceTest {
 
